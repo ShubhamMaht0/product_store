19498b54ea6e6844cab7bf5d4dd84023744e9bf8
Gamma <shubhammahto2033@gmail.com>
2025-06-10 23:24:28 +0530

    alias config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import jsconfigPaths from "vite-jsconfig-paths"
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server:{
     proxy:{
       "/api":{
